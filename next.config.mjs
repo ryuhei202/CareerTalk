d5356fb058cd87bf11a0031a2e45b045092b97ca
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-10-13 13:06:41 +0900

    add: 画像の外部ドメイン設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+// 外部画像のドメイン許可設定
+// @see https://dev.classmethod.jp/articles/tsnote-cognito-change-password-error/
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'profile.line-scdn.net',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
