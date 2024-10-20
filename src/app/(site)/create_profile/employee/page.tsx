ec06e7e4e1e0f4197fb202212c9d41ffe8386a62
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-10-20 14:18:20 +0900

    test: テスト用のフロントを作成

--- src/app/(site)/create_profile/employee/page.tsx
+++ src/app/(site)/create_profile/employee/page.tsx
@@ -1,22 +1,52 @@
 import { getServerSession } from "@/lib/auth";
-import { prisma } from "@/lib/prisma";
+import TestUseCaseButton from "./_components/TestUseCaseButton";
+import { registerContainer } from "@/di/container";
+import { REGISTER_EMPLOYEE_USE_CASE } from "@/di/container/usecase";
+import { RegisterEmployeeUseCase } from "@/application/usecase/registerEmployee";
+import { RegisterEmployeeParams } from "@/application/usecase/registerEmployee";
+import {
+  GenderEnum,
+  HiringTypeEnum,
+  MeetingMethodEnum,
+} from "@/domain/core/Employee/Employee";
 
 export default async function CreateProfileEmployee() {
   const session = await getServerSession();
   const userId = session?.user.id;
 
-  // ここをusecase層に移行する
-  if (userId) {
-    const employee = await prisma.employee.findUnique({
-      where: { userId: userId },
-    });
-
-    if (!employee) {
-      await prisma.employee.create({
-        data: { userId: userId },
-      });
-    }
+  console.log("通った！");
+  if (!userId) {
+    return <div>ログインしてください</div>;
   }
+  const container = registerContainer();
+  const useCase = container.get<RegisterEmployeeUseCase>(
+    REGISTER_EMPLOYEE_USE_CASE
+  );
 
-  return <div>employee create_profile</div>;
+  const useCaseParams: RegisterEmployeeParams = {
+    userId: userId,
+    name: "test",
+    companyCode: "12345679",
+    occupationId: 1,
+    gender: GenderEnum.MALE,
+    birthday: new Date("1999-06-15"),
+    joiningDate: new Date("2022-04-01"),
+    workLocationId: 1,
+    hiringType: HiringTypeEnum.NEW_GRADUATE,
+    meetingMethod: MeetingMethodEnum.ONLINE,
+    selfIntroduction: "test",
+    profileImage: "test",
+    talkableTopics: "test",
+  };
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const useCaseResult = await useCase(useCaseParams);
+
+  console.log(useCaseResult);
+  return (
+    <>
+      <div>employee create_profile</div>
+      {/* {userId && <TestUseCaseButton userId={userId} />} */}
+    </>
+  );
 }
--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,61 +3,70 @@ import { Button } from "@/app/_components/ui/button";
 import { MessageCircle, Users, MessageSquare, Calendar } from "lucide-react";
 import { LogOutButton } from "../_components/parts/Button/LogOutButton";
 
-export default async function SiteLayout() {
+export default async function SiteLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <header className="bg-white shadow-sm">
-      <div className="container mx-auto px-4 py-4">
-        <nav className="flex justify-between items-center">
-          <Link href="/" className="flex items-center space-x-2">
-            <MessageCircle className="h-8 w-8 text-purple-600" />
-            <span className="text-2xl font-bold text-gray-800">CareerTalk</span>
-          </Link>
-          <ul className="hidden md:flex space-x-6 items-center">
-            <li>
-              <Link
-                href="/"
-                className="text-gray-600 hover:text-purple-600 flex items-center"
-              >
-                <MessageCircle className="h-5 w-5 mr-1" />
-                ホーム
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/employees"
-                className="text-gray-600 hover:text-purple-600 flex items-center"
-              >
-                <Users className="h-5 w-5 mr-1" />
-                現場社員一覧
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/messages"
-                className="text-gray-600 hover:text-purple-600 flex items-center"
-              >
-                <MessageSquare className="h-5 w-5 mr-1" />
-                DM
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/events"
-                className="text-gray-600 hover:text-purple-600 flex items-center"
-              >
-                <Calendar className="h-5 w-5 mr-1" />
-                イベント
-              </Link>
-            </li>
-            <li>
-              <LogOutButton />
-            </li>
-          </ul>
-          <Button className="md:hidden" variant="ghost">
-            メニュー
-          </Button>
-        </nav>
-      </div>
-    </header>
+    <>
+      <header className="bg-white shadow-sm">
+        <div className="container mx-auto px-4 py-4">
+          <nav className="flex justify-between items-center">
+            <Link href="/" className="flex items-center space-x-2">
+              <MessageCircle className="h-8 w-8 text-purple-600" />
+              <span className="text-2xl font-bold text-gray-800">
+                CareerTalk
+              </span>
+            </Link>
+            <ul className="hidden md:flex space-x-6 items-center">
+              <li>
+                <Link
+                  href="/"
+                  className="text-gray-600 hover:text-purple-600 flex items-center"
+                >
+                  <MessageCircle className="h-5 w-5 mr-1" />
+                  ホーム
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/employees"
+                  className="text-gray-600 hover:text-purple-600 flex items-center"
+                >
+                  <Users className="h-5 w-5 mr-1" />
+                  現場社員一覧
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/messages"
+                  className="text-gray-600 hover:text-purple-600 flex items-center"
+                >
+                  <MessageSquare className="h-5 w-5 mr-1" />
+                  DM
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/events"
+                  className="text-gray-600 hover:text-purple-600 flex items-center"
+                >
+                  <Calendar className="h-5 w-5 mr-1" />
+                  イベント
+                </Link>
+              </li>
+              <li>
+                <LogOutButton />
+              </li>
+            </ul>
+            <Button className="md:hidden" variant="ghost">
+              メニュー
+            </Button>
+          </nav>
+        </div>
+      </header>
+      {children}
+    </>
   );
 }
