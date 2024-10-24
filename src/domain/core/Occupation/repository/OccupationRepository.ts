a82caff4f55c510b3126f1efbce73db2b6254a23
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-10-24 19:05:25 +0900

    delete: サブドメインのモデルを全部削除

--- src/domain/core/Occupation/repository/OccupationRepository.ts
+++
@@ -1,7 +0,0 @@
-import type { Result } from "@/util/result";
-import type { Occupation } from "../Occupation";
-
-export type FindOccupationResult = Result<Occupation | undefined, never>;
-export interface OccupationRepository {
-	findById(occupationId: number): Promise<FindOccupationResult>;
-}
--- src/domain/core/WorkLocation/repository/WorkLocationRepository.ts
+++
@@ -1,7 +0,0 @@
-import type { Result } from "@/util/result";
-import type { WorkLocation } from "../WorkLocation";
-
-export type FindWorkLocationResult = Result<WorkLocation | undefined, never>;
-export interface WorkLocationRepository {
-	findById(workLocationId: number): Promise<FindWorkLocationResult>;
-}
