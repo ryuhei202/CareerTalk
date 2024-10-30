ba9f6f3c62649f34a68cb8b632bcfec58271f45f
ShuntaToda <todashuntawork@gmail.com>
2024-10-30 19:13:53 +0900

    feat employeeの取得

--- src/app/(site)/employee/my_page/page.tsx
+++ src/app/(site)/employee/my_page/page.tsx
@@ -1,5 +1,20 @@
+import { getServerSession } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 export default async function MyPage() {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/signin");
+  }
+  const employee = await prisma.employee.findUnique({
+    where: {
+      userId: session.user.id,
+    },
+    include: {
+      user: true,
+    },
+  });
 
   return (
     <>
