ffa3bb4be8a8d0189e062eea4e929161c8231041
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-11-01 00:54:13 +0900

    既に会員登録している場合はマッチング画面に移動するようにする

--- src/app/(site)/applicant/create_profile/page.tsx
+++ src/app/(site)/applicant/create_profile/page.tsx
@@ -1,5 +1,6 @@
-import { getServerSession } from "@/lib/auth";
+import { getApplicantUserId, getServerSession } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import CreateProfileApplicant from "./_components/CreateApplicantProfileContainer";
 
 export type TOccupation = {
@@ -9,6 +10,11 @@ export type TOccupation = {
 
 export default async function CreateProfileApplicantPage() {
   const session = await getServerSession();
+  const applicantUserId = await getApplicantUserId();
+  // すでに会員登録済みの場合は現場社員検索画面にリダイレクト
+  if (applicantUserId) {
+    redirect("/applicant/search_employees");
+  }
   const occupations: TOccupation[] = await prisma.occupation.findMany();
   return (
     <CreateProfileApplicant
--- src/app/(site)/employee/create_profile/page.tsx
+++ src/app/(site)/employee/create_profile/page.tsx
@@ -1,5 +1,6 @@
-import { getServerSession } from "@/lib/auth";
+import { getEmployeeUserId, getServerSession } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import CreateEmployeeProfileContainer from "./_components/CreateEmplopyeeProfileContainer";
 
 export type TOccupation = {
@@ -9,6 +10,11 @@ export type TOccupation = {
 
 export default async function CreateProfileEmployeePage() {
   const session = await getServerSession();
+  const employeeUserId = await getEmployeeUserId();
+  // すでに会員登録済みの場合はいいね一覧画面にリダイレクト
+  if (employeeUserId) {
+    redirect("/employee/matches");
+  }
   const occupations: TOccupation[] = await prisma.occupation.findMany();
   return (
     <CreateEmployeeProfileContainer
