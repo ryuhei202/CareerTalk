d76f7a1620179e9959ffa77d742e2411d68e0464
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-11-12 03:11:19 +0900

    fix

--- src/app/(site)/employee/matches/_components/LikeApplicationsContainer.tsx
+++ src/app/(site)/employee/matches/_components/LikeApplicationsContainer.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useSearchParamsUpdate } from "@/app/(site)/applicant/search_employees/_util/useSearchParamsUpdate";
 import type { LikedApplicant } from "@/usecase/getLikedApplicants/getLikedApplicantsUseCase";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import LikedApplicantList from "./LikedApplicantList";
 
 export default function LikeApplicationsContainer({
@@ -10,8 +12,12 @@ export default function LikeApplicationsContainer({
   totalCount: number;
   applicants: LikedApplicant[];
 }) {
+  const router = useRouter();
   const { handleChangePage, currentPage } = useSearchParamsUpdate();
   const totalPages = Math.ceil(totalCount / 10);
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
   return (
     <div className="flex min-h-screen bg-gray-100">
       {/* Main Content */}
