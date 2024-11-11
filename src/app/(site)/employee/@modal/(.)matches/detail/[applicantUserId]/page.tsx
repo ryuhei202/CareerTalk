d352731c13c2adb9f24120409dc6594a1f2e90f4
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-11-12 02:01:10 +0900

    test

--- src/app/(site)/employee/@modal/(.)matches/detail/[applicantUserId]/page.tsx
+++ src/app/(site)/employee/@modal/(.)matches/detail/[applicantUserId]/page.tsx
@@ -1,4 +1,6 @@
+import ApplicantCardContainer from "@/app/(site)/employee/matches/detail/[applicantUserId]/_components/ApplicantCardContainer";
 import ErrorPage from "@/app/_components/page/ErrorPage";
+import { Modal } from "@/app/_components/parts/Modal";
 import { getEmployeeUserId } from "@/lib/auth";
 import {
   type GetLikedApplicantDetailUseCaseResult,
@@ -42,10 +44,12 @@ export default async function ApplicantDetailPage({ params }: Props) {
   }
 
   return (
-    <ApplicantDetailModal
-      applicant={result.data.applicant}
-      likeReason={result.data.likeReason}
-      likeMessage={result.data.likeMessage}
-    />
+    <Modal>
+      <ApplicantCardContainer
+        applicant={result.data.applicant}
+        likeReason={result.data.likeReason}
+        likeMessage={result.data.likeMessage}
+      />
+    </Modal>
   );
 }
