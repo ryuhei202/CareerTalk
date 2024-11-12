8b860710f1c3dfc679611f2d245b44a4c5798195
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-11-12 20:33:36 +0900

    fix

--- src/app/(site)/employee/@modal/(.)matches/detail/[applicantUserId]/page.tsx
+++ src/app/(site)/employee/@modal/(.)matches/detail/[applicantUserId]/page.tsx
@@ -23,6 +23,7 @@ export default async function ApplicantDetailPage({ params }: Props) {
   if (!employeeUserId) {
     redirect("/employee/create_profile");
   }
+  console.log("fix");
 
   const getLikedApplicantDetailUseCaseParams: GetLikedApplicantDetailParams = {
     applicantUserId: params.applicantUserId,
