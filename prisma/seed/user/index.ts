1e7d11ba82b52d31d9612096bb0a3143980f5331
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-11-22 19:27:35 +0900

    fix: テストデータに画像を入れる

--- prisma/seed/user/index.ts
+++ prisma/seed/user/index.ts
@@ -2,9 +2,26 @@ import type { PrismaPromise, User } from "@prisma/client";
 import { prisma } from "..";
 import fixture from "./fixture.json";
 
+const rootUrl = process.env.EMPLOYEE_IMAGE_CLOUDFRONT_URL;
+const userImages = [
+	`${rootUrl}/man/IMG_1043 (1).jpg`,
+	`${rootUrl}/man/IMG_1043.jpg`,
+	`${rootUrl}/man/IMG_1044.jpg`,
+	`${rootUrl}/man/IMG_1045.jpg`,
+	`${rootUrl}/woman/IMG_1038.jpg`,
+	`${rootUrl}/woman/IMG_1040.jpg`,
+	`${rootUrl}/woman/IMG_1041.jpg`,
+	`${rootUrl}/woman/IMG_1042.jpg`,
+];
+
+const usersWithImage = fixture.map((user, index) => ({
+	...user,
+	image: userImages[index % userImages.length],
+}));
+
 export const user = () => {
 	const res: PrismaPromise<User>[] = [];
-	for (const data of fixture) {
+	for (const data of usersWithImage) {
 		const row = prisma.user.upsert({
 			where: { id: data.id },
 			update: data,
