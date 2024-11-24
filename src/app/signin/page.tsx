a974895e5118a6caa7e77b50ba3c85736d9d8636
ShuntaToda <todashuntawork@gmail.com>
2024-11-25 07:23:06 +0900

    feat queryparamでログインのタイプを選択

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -8,44 +8,80 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/app/_components/ui/card";
+import { useSearchParams } from "next/navigation";
 import { useState } from "react";
+import { RoundedPillButton } from "../_components/ui/RoundedPillButton";
+import { Button } from "../_components/ui/button";
 
 export default function SignInPage() {
-	const [isEmployee, setIsEmployee] = useState<boolean>(false);
+	const searchParams = useSearchParams()
+	const [isEmployee, setIsEmployee] = useState<boolean>(() => {
+		if (searchParams.get("is_employee") === "true") {
+			return true;
+		}
+		return false;
+	});
+	const [isSelectedUserType, setIsSelectedUserType] = useState<boolean>(() => {
+		if (searchParams.get("is_employee") === "true" || searchParams.get("is_employee") === "false") {
+			return true;
+		}
+		return false;
+
+	});
 	return (
-		<div className="min-h-screen bg-gradient-to-br from-purple-50 to-pink-50 flex items-center justify-center p-4">
-			<Card className="w-full max-w-md py-8">
-				<CardHeader className="space-y-1 pb-8">
-					<CardTitle className="text-2xl font-bold text-center">
-						CareerTalk にログイン
-					</CardTitle>
-					<CardDescription className="text-center">
-						{isEmployee
-							? "現場社員の方はこちらからログインしてください"
-							: "転職希望者の方はこちらからログインしてください"}
-					</CardDescription>
-				</CardHeader>
-				<CardContent className="space-y-8 flex flex-col items-center">
-					<SocialLoginButton
-						callbackUrl={
-							isEmployee
-								? "/employee/create_profile"
-								: "/applicant/create_profile"
-						}
-					/>
-				</CardContent>
-				<CardFooter className="flex flex-col space-y-2 pt-4">
-					<div className="text-sm text-center">
-						<button
-							type="button"
-							onClick={() => setIsEmployee(!isEmployee)}
-							className="text-purple-600 hover:underline"
-						>
-							{isEmployee ? "転職希望者の方はこちら" : "現場社員の方はこちら"}
-						</button>
-					</div>
-				</CardFooter>
-			</Card>
+		<div className="min-h-screen bg-muted flex items-center justify-center p-4">
+			{isSelectedUserType ? (
+				<Card className="w-full max-w-md py-8">
+					<CardHeader className="space-y-1 pb-8">
+						<CardTitle className="text-2xl font-bold text-center">
+							ハイキャリトークにログイン
+						</CardTitle>
+						<CardDescription className="text-center">
+							{isEmployee
+								? "現場社員の方"
+								: "転職希望者の方"}
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="space-y-8 flex flex-col items-center">
+						<SocialLoginButton
+							callbackUrl={
+								isEmployee
+									? "/employee/create_profile"
+									: "/applicant/create_profile"
+							}
+						/>
+					</CardContent>
+					<CardFooter className="flex flex-col space-y-2 pt-4">
+						<div className="text-sm text-center">
+							<Button
+								onClick={() => setIsSelectedUserType(false)}
+								variant={"outline"}
+							>
+								利用タイプを変更する
+							</Button>
+						</div>
+					</CardFooter>
+				</Card>
+			) : (
+				<Card className="w-full max-w-md py-8">
+					<CardHeader className="space-y-1 pb-8">
+						<CardTitle className="text-2xl font-bold text-center">
+							どちらの利用者ですか？
+						</CardTitle>
+					</CardHeader>
+					<CardContent className="space-y-8 flex flex-col items-center">
+						<RoundedPillButton className="w-full" onClick={() => {
+							setIsSelectedUserType(true)
+							setIsEmployee(false)
+						}}>転職希望者</RoundedPillButton>
+						<RoundedPillButton variant={"white"} className="shadow-lg w-full" onClick={() => {
+							setIsSelectedUserType(true)
+							setIsEmployee(true)
+						}}>現場社員</RoundedPillButton>
+					</CardContent>
+				</Card>
+
+			)}
 		</div>
 	);
 }
