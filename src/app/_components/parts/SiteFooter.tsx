880b144ca0d4023002ad1866fb4c59667088424a
ShuntaToda <todashuntawork@gmail.com>
2024-11-25 06:55:14 +0900

    feat プライバシーポリシーと利用規約の表示

--- src/app/_components/parts/SiteFooter.tsx
+++ src/app/_components/parts/SiteFooter.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
+
 export function SiteFooter() {
   return (
     <footer className="border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          普通は1番下に企業情報、運営規約、プライバシーポリシー等が入るけど、何か入れる？
-        </p>
+        <Link href={"/privacy_policy"} className="border-b hover:text-gray-500">プライバシーポリシー</Link>
+        <Link href={"/terms_of_service"} className="border-b hover:text-gray-500">利用規約</Link>
       </div>
     </footer>
   )
