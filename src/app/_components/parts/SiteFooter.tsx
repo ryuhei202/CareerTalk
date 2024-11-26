563801689b113e4eeab70e5ba99cf21785f16ead
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-11-26 22:15:10 +0900

    fix: fotterのレイアウト修正

--- src/app/_components/parts/SiteFooter.tsx
+++ src/app/_components/parts/SiteFooter.tsx
@@ -2,11 +2,18 @@ import Link from "next/link";
 
 export function SiteFooter() {
   return (
-    <footer className="border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-        <Link href={"/privacy_policy"} className="border-b hover:text-gray-500">プライバシーポリシー</Link>
-        <Link href={"/terms_of_service"} className="border-b hover:text-gray-500">利用規約</Link>
+    <footer className="border-t border-border/40 py-6 dark:border-border">
+      <div className="flex justify-center items-center gap-8">
+        <Link href={"/privacy_policy"} className="border-b hover:text-gray-500">
+          プライバシーポリシー
+        </Link>
+        <Link
+          href={"/terms_of_service"}
+          className="border-b hover:text-gray-500"
+        >
+          利用規約
+        </Link>
       </div>
     </footer>
-  )
+  );
 }
